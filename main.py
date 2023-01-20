d675aed5ac267c0605c80c277e27eb5038970ab2
Учебная практика <Practice@op9.local>
2023-01-20 14:03:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import warnings
+
+warnings.simplefilter("ignore")
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pylab import rcParams
+
+rcParams["figure.figsize"] = 8, 5
+import pandas as pd
+
